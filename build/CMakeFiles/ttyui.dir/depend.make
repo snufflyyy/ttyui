d03bc0e30af6fd103ae5d6ec3f3f8b3e2337bd66
braden <snufflyy@proton.me>
2025-09-28 14:31:55 -0500

    Delete build directory

--- build/CMakeFiles/ttyui.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for ttyui.
-# This may be replaced when dependencies are built.
--- build/CMakeFiles/ttyui.dir/src/types/vector2.c.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/ttyui.dir/src/types/vector2.c.o: \
- /home/braden/Programming/C/ttyui/src/types/vector2.c \
- /usr/include/stdc-predef.h \
- /home/braden/Programming/C/ttyui/include/types/vector2.h
